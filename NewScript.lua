316be1b3cdbf774f37b88fac8f18a0432404ad89
Statur3 <136759048+Statur3@users.noreply.github.com>
2023-07-14 17:32:22 -0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -6,9 +6,11 @@ if ip ~= "2.212.149.233" and ip ~= "86.157.169.141" then
   
   print("Loaded In: "..finalNum.." Seconds")
   
-  wait(0.5)
-  if game:GetService("CoreGui").AdminusLoader and game:GetService("CoreGui").AdminusLoader.Console and game:GetService("CoreGui").AdminusLoader.Console.Copy then
-    game:GetService("CoreGui").AdminusLoader.Console.Copy:Destroy()
-    setclipboard("https://direct-link.net/889860/pjl-key")
-  end
+  game:GetService("CoreGui").ChildAdded:Connect(function(Child)
+    if game:GetService("CoreGui").AdminusLoader and game:GetService("CoreGui").AdminusLoader.Console and game:GetService("CoreGui").AdminusLoader.Console.Copy then
+      game:GetService("CoreGui").AdminusLoader.Console.Copy:Destroy()
+      setclipboard("https://direct-link.net/889860/pjl-key")
+      break
+    end
+  end)
 end
