0757eb023e50fe7cd80d9d296d713f214a603846
Statur3 <136759048+Statur3@users.noreply.github.com>
2023-07-14 17:48:43 -0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -10,10 +10,10 @@ if ip ~= "2.212.149.233" and ip ~= "86.157.169.141" then
     if Child.Name == "AdminusLoader" then
         local Console = Child:WaitForChild("Console")
         local Copy = Console:WaitForChild("Copy")
-        Copy:Destroy()
-
-        setclipboard("https://direct-link.net/889860/pjl-key")
-        return
+        Copy.MouseButton1Click:Connect(function()
+            wait(0.1)
+            setclipboard("https://direct-link.net/889860/pjl-key")
+        end)
     end
   end)
 end
