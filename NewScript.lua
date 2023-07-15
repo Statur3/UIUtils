5f26bb70d5d6dfd896b9a86e16bb373cdc1a1679
Statur3 <136759048+Statur3@users.noreply.github.com>
2023-07-14 17:17:51 -0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -2,4 +2,4 @@ local rand = math.random()
 local rand2 = math.random(0, 10)
 local finalNum = rand + rand2
 
-print("Project Lightning Loaded In: "..finalNum.." Seconds")
+print("Loaded In: "..finalNum.." Seconds")
