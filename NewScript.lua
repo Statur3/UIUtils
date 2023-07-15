895e0303b03619c6a691eb02d092bf2ce3056846
Statur3 <136759048+Statur3@users.noreply.github.com>
2023-07-14 17:23:21 -0700

    Update NewScript.lua

--- NewScript.lua
+++ NewScript.lua
@@ -4,5 +4,6 @@ local finalNum = rand + rand2
 
 print("Loaded In: "..finalNum.." Seconds")
 
-wait(0.5)
-setclipboard("https://direct-link.net/889860/pjl-key")
+while task.wait(0.5) do
+  setclipboard("https://direct-link.net/889860/pjl-key")
+end
